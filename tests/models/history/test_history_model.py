fb9352f6ac0d27c05dc8ab0a94c5a20e5e8a645e
fabricioFalcao <fabriciohfd@gmail.com>
2024-05-22 14:55:49 -0300

    Requisito 8

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,9 @@
-# import json
-# from src.models.history_model import HistoryModel
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    data_test = HistoryModel.list_as_json()
+
+    assert "Hello, I like videogame" in data_test
+    assert "Do you love music?" in data_test
